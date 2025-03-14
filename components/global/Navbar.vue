9c89d2c9a20385c130c8d4f16e2650e518743c19
rockteck57 <oleksandrkhomenko57@gmail.com>
2025-03-14 16:47:31 +0000

    updt

--- components/global/Navbar.vue
+++ components/global/Navbar.vue
@@ -58,7 +58,7 @@ onBeforeUnmount(() => {
                 <img class="w-40" src="public/images/VeluxeAutoLogo.png" alt="Logo Image">
               </NuxtLink>
             </div>
-            <div class="flex justify-center items-center gap-10 font-medium text-[13px]">
+            <div class="flex justify-center items-center gap-3 sm:gap-10 font-medium text-[13px]">
                 <NuxtLink
                 :to="localCode=='pt' ? '/stock' : '/'+localCode+'/stock'" 
                 active-class="border-[#b53d3d] text-[#b53d3d] border-t-2"
@@ -67,7 +67,7 @@ onBeforeUnmount(() => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24"><path fill="currentColor" d="M20.515 13.754a.886.886 0 0 1-.88.88h-3.548a.885.885 0 0 1-.88-.88a.886.886 0 0 1 .88-.88h3.548a.89.89 0 0 1 .88.88m-12.376 0a.885.885 0 0 1-.88.88H3.711a.885.885 0 0 1-.88-.88a.886.886 0 0 1 .88-.88h3.548a.886.886 0 0 1 .879.88zm-1.84-8.167h11.404l1.399 3.562l-.069-.004H4.899zm17.68 2.706a1.33 1.33 0 0 0-1.527-1.094l.008-.001l-2.183.356a1 1 0 0 0-.094.026l.005-.002L18.782 4H5.216L3.81 7.578a1 1 0 0 0-.087-.023l-2.185-.357A1.33 1.33 0 0 0 .019 8.286l-.001.008a1.33 1.33 0 0 0 1.088 1.519l.008.001l1.271.209a4.23 4.23 0 0 0-1.3 2.955v6.091h4.4v-2.3h12.429v2.3h4.4v-3.248c.018-.076.028-.163.028-.253v-2.586a4.24 4.24 0 0 0-1.213-2.876l.001.001l1.766-.29a1.33 1.33 0 0 0 1.092-1.527l.001.008z"/></svg>
                     {{ t('stock') }}
                 </NuxtLink>
-                <button @click="scrollToBottom()" class="flex flex-col justify-center gap-1 items-center hover:text-[#b53d3d] transition duration-300 ease-in-out border-t-2 border-transparent hover:border-[#b53d3d] pt-2">
+                <button @click="scrollToBottom()" class="flex flex-col justify-center gap-1 items-center hover:text-[#b53d3d] transition duration-300 ease-in-out border-t-2 border-transparent hover:border-[#b53d3d] pt-1.5">
                     <i class="fa-solid fa-phone text-[19px] py-[5px]"></i>
                     <span class="text-[12px]">{{ t('contacts') }}</span>
                 </button>
