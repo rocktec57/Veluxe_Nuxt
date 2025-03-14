8d015f6c556256617d03b4bdf82bbbcdf80625c0
rockteck57 <oleksandrkhomenko57@gmail.com>
2025-03-14 15:47:45 +0000

    icon

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 useHead({
+  title: "Veluxe Auto | Showroom",
   link: [
     {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap'
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap"
     }
   ]
 });
